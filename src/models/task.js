51585e52bd7065edc0c1dfb0e4fc0a7e25425878
Mohamed Ismail <mohamedismail77@yahoo.com>
2021-12-15 09:53:38 +0200

    first commit

---
+++ src/models/task.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+
+
+const Task = mongoose.model('Task',{
+    description:{
+        type: String,
+        trim: true,
+        required: true
+    },
+    status:{
+        type: Boolean,
+        default: false
+    }
+})
+ 
+
+module.exports = Task
